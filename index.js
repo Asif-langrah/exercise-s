e18ca3c5c0ad050cd2565de03e41e47b594888c7
Mian Asif Langrah <143902006+Asif-langrah@users.noreply.github.com>
2023-10-31 17:24:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//store name in variable and print message to name
+let personName = `Asif`;
+let message = `Hellow ${personName}, hope you are doing well`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,5 @@
+//store name in variable and print message to name
+
+let personName:string=`Asif`
+let message:string=`Hellow ${personName}, hope you are doing well`
+console.log(message);
